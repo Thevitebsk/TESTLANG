24e77c5d06d004dc683c0e5e02d158f2eac2e8b1
Gaham <155390134+Thevitebsk@users.noreply.github.com>
2024-08-05 10:59:32 +0300

    Add files via upload

---
+++ Every.py
@@ -0,0 +1,35 @@
+i=input(">>")
+c=i.split()
+cmd=0
+s=[]
+inp=''
+print(c)
+while len(c)!=0:
+    cmd+=1
+    if c[0]=="out":
+        print(end=c[1])
+        c.pop(0)
+        c.pop(0)
+    elif c[0]=="note":
+        c.pop(0)
+        c.pop(0)
+    elif c[0]=="in":
+        inp=input("input\n")
+        c.pop(0)
+    elif c[0]=="push":
+        if c[1]=="in":
+            s.append(inp)
+            c.pop(0)
+            c.pop(0)
+        if c[1]=="out":
+            print(s[0])
+            c.pop(0)
+            c.pop(0)
+            s.pop(0)
+        else:
+            s.append(c[1])
+            c.pop(0)
+            c.pop(0)
+    else:
+        print(f"CMD:{cmd} UNDEFINED COMMAND")
+        break
