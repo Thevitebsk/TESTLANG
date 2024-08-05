e217863a2e9828d24d8fc2782baf5c665f88b094
Gaham <155390134+Thevitebsk@users.noreply.github.com>
2024-08-05 13:11:25 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ c=i.split()
 cmd=0
 s=[]
 inp=''
-print(c)
 while len(c)!=0:
     cmd+=1
     if c[0]=="out":
@@ -22,7 +21,7 @@ while len(c)!=0:
             c.pop(0)
             c.pop(0)
         elif c[1]=="out":
-            print(end=s[0])
+            print(end=str(s[0]))
             c.pop(0)
             c.pop(0)
             s.pop(0)
@@ -32,6 +31,14 @@ while len(c)!=0:
             c.pop(0)
         else:
             s.append(c[1])
+            c.pop(0)
+            c.pop(0)
+    elif c[0]=="op":
+        if c[1]=="plus":
+            r=int(s[0])+int(s[1])
+            s.append(r)
+            s.pop(0)
+            s.pop(0)
             c.pop(0)
             c.pop(0)
     else:
